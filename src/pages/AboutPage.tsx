1d8c9028b59a8a0ebe9d0e60b97205b62ffee1a9
birando <birger1@live.com>
2024-12-27 13:43:21 +0100

    HTML/React: add sections and pages to components Ref: #9

---
+++ src/pages/AboutPage.tsx
@@ -0,0 +1,41 @@
+
+export default function AboutPage() {
+  return (
+    <div className="width-container u-flex-column">
+      <section className="about-section">
+        <h1 className="about-section__heading">ABOUT US</h1>
+        <article className="about-section__content">
+          <div className="about-section__text-area">
+            <p className="about-section__body-text">
+              <b>Welcome to Plant Plaza</b>, your go-to destination for unique
+              and captivating plants. Whether you're a seasoned plant enthusiast
+              or just starting your indoor jungle, we believe every space
+              deserves a touch of greenery and charm.
+            </p>
+            <p className="about-section__body-text">
+              <b>Our carefully curated collection</b> features rare, exotic, and
+              easy-care plants, handpicked to inspire and elevate your
+              environment. From vibrant succulents to statement-making
+              tropicals, we strive to bring the beauty of nature into your home
+              or workspace.
+            </p>
+            <p className="about-section__body-text">
+              <b>At Plant Plaza</b>, we're not just about selling plants; we're
+              about cultivating connections. Join our growing community of plant
+              lovers and discover tips, inspiration, and everything you need to
+              nurture your new green companions.
+            </p>
+            <p className="about-section__body-text">
+              <b>Let’s grow together! 🌱</b>
+            </p>
+          </div>
+          <img
+            className="about-section__image"
+            src="./src/assets/images/plantAbout.jpg"
+            alt=""
+          />
+        </article>
+      </section>
+    </div>
+  );
+}
---
+++ src/pages/CartPage.tsx
@@ -0,0 +1,188 @@
+
+export default function CartPage() {
+  return (
+    <div className="width-container u-flex-column">
+      <section className="cart-section">
+        <h1 className="cart-section__heading">CART</h1>
+        <div className="cart-section__content">
+          <div className="cart-section__card">
+            <img
+              className="cart-card__image"
+              src="./src/assets/images/blueFlower.png"
+              alt="product: blue flower"
+            />
+            <div className="cart-card__container">
+              <h3 className="cart-card__heading">Blue Flower</h3>
+              <div className="cart-card__counter">
+                <button
+                  type="button"
+                  className="cart-card__delete button--filled"
+                >
+                  Del
+                </button>
+                <div className="cart-card__count">0</div>
+                <button type="button" className="cart-card__add button--filled">
+                  Add
+                </button>
+              </div>
+            </div>
+          </div>
+          <div className="cart-section__card">
+            <img
+              className="cart-card__image"
+              src="./src/assets/images/tulipSmall.png"
+              alt="product: tulip"
+            />
+            <div className="cart-card__container">
+              <h3 className="cart-card__heading">Tulip</h3>
+              <div className="cart-card__counter">
+                <button
+                  type="button"
+                  className="cart-card__delete button--filled"
+                >
+                  Del
+                </button>
+                <div className="cart-card__count">0</div>
+                <button type="button" className="cart-card__add button--filled">
+                  Add
+                </button>
+              </div>
+            </div>
+          </div>
+          <div className="cart-section__card">
+            <img
+              className="cart-card__image"
+              src="./src/assets/images/purpleFlowerSmall.png"
+              alt="product: purple flower"
+            />
+            <div className="cart-card__container">
+              <h3 className="cart-card__heading">Purple Flower</h3>
+              <div className="cart-card__counter">
+                <button
+                  type="button"
+                  className="cart-card__delete button--filled"
+                >
+                  Del
+                </button>
+                <div className="cart-card__count">0</div>
+                <button type="button" className="cart-card__add button--filled">
+                  Add
+                </button>
+              </div>
+            </div>
+          </div>
+          <div className="cart-section__card">
+            <img
+              className="cart-card__image"
+              src="./src/assets/images/mandalaPlant.png"
+              alt="product: mandala plant"
+            />
+            <div className="cart-card__container">
+              <h3 className="cart-card__heading">Mandala Plant</h3>
+              <div className="cart-card__counter">
+                <button
+                  type="button"
+                  className="cart-card__delete button--filled"
+                >
+                  Del
+                </button>
+                <div className="cart-card__count">0</div>
+                <button type="button" className="cart-card__add button--filled">
+                  Add
+                </button>
+              </div>
+            </div>
+          </div>
+          <div className="cart-section__card">
+            <img
+              className="cart-card__image"
+              src="./src/assets/images/birdFoot.png"
+              alt="product: bird foot"
+            />
+            <div className="cart-card__container">
+              <h3 className="cart-card__heading">Bird Foot</h3>
+              <div className="cart-card__counter">
+                <button
+                  type="button"
+                  className="cart-card__delete button--filled"
+                >
+                  Del
+                </button>
+                <div className="cart-card__count">0</div>
+                <button type="button" className="cart-card__add button--filled">
+                  Add
+                </button>
+              </div>
+            </div>
+          </div>
+          <div className="cart-section__card">
+            <img
+              className="cart-card__image"
+              src="./src/assets/images/blueFrog.png"
+              alt="product: blue frog"
+            />
+            <div className="cart-card__container">
+              <h3 className="cart-card__heading">Blue Frog</h3>
+              <div className="cart-card__counter">
+                <button
+                  type="button"
+                  className="cart-card__delete button--filled"
+                >
+                  Del
+                </button>
+                <div className="cart-card__count">0</div>
+                <button type="button" className="cart-card__add button--filled">
+                  Add
+                </button>
+              </div>
+            </div>
+          </div>
+          <div className="cart-section__card">
+            <img
+              className="cart-card__image"
+              src="./src/assets/images/pinkPrincess.png"
+              alt="product: pink princess"
+            />
+            <div className="cart-card__container">
+              <h3 className="cart-card__heading">Pink Princess</h3>
+              <div className="cart-card__counter">
+                <button
+                  type="button"
+                  className="cart-card__delete button--filled"
+                >
+                  Del
+                </button>
+                <div className="cart-card__count">0</div>
+                <button type="button" className="cart-card__add button--filled">
+                  Add
+                </button>
+              </div>
+            </div>
+          </div>
+          <div className="cart-section__card">
+            <img
+              className="cart-card__image"
+              src="./src/assets/images/kingsball.png"
+              alt="product: king's ball"
+            />
+            <div className="cart-card__container">
+              <h3 className="cart-card__heading">King's Ball</h3>
+              <div className="cart-card__counter">
+                <button
+                  type="button"
+                  className="cart-card__delete button--filled"
+                >
+                  Del
+                </button>
+                <div className="cart-card__count">0</div>
+                <button type="button" className="cart-card__add button--filled">
+                  Add
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </div>
+  );
+}
